520ffe3735b5dd3bbe05f3b114e9dacb7615e9d5
[titouan] <[titouannicolasgauze@gmail.com]>
2024-11-26 11:33:05 +0100

    modif base phpMyAdmin

--- BD/sql/basePhpMySql.sql
+++ BD/sql/basePhpMySql.sql
@@ -1,157 +1,174 @@
+-- Suppression des tables existantes
 DROP TABLE IF EXISTS Avis;
 DROP TABLE IF EXISTS ARegarde;
 DROP TABLE IF EXISTS ACommande;
 DROP TABLE IF EXISTS Commande;
-DROP TABLE IF EXISTS MethodeEnregistrer;
+DROP TABLE IF EXISTS infoPaiement;
 DROP TABLE IF EXISTS Client;
-DROP TABLE IF EXISTS Comporte;
-DROP TABLE IF EXISTS Image;
-DROP TABLE IF EXISTS Produit;
-DROP TABLE IF EXISTS MethodePaiement;
-DROP TABLE IF EXISTS Categorie;
 DROP TABLE IF EXISTS Employe;
+DROP TABLE IF EXISTS AdressePostale;
+DROP TABLE IF EXISTS Comporte;
+DROP TABLE IF EXISTS Contient;
+DROP TABLE IF EXISTS Produit;
+DROP TABLE IF EXISTS Categorie;
+DROP TABLE IF EXISTS Image;
+DROP TABLE IF EXISTS Carte_AE;
+DROP TABLE IF EXISTS Carte_EU;
+DROP TABLE IF EXISTS Paypal;
+DROP TABLE IF EXISTS MethodePaiement;
 
-CREATE TABLE Employe (
-    idNumEmployer INT(5),
-    nom VARCHAR(25),
-    prenom VARCHAR(15),
-    adressePostale VARCHAR(50),
-    email VARCHAR(20),
-    telephone CHAR(10),
-    administrateur TINYINT(1),
-    CONSTRAINT pk_Employe PRIMARY KEY (idNumEmployer)
-);
-
-CREATE TABLE Categorie (
-    idCategorie INT(3),
-    nomTypeCategorie VARCHAR(30),
-    idCategoriePere INT(3),
-    CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie),
-    CONSTRAINT fk_Categorie_Categorie FOREIGN KEY (idCategoriePere) REFERENCES Categorie(idCategorie)
-);
-
+-- Création des tables
 CREATE TABLE MethodePaiement (
-    idMethodePaiement INT(6),
-    nomMethodePaiement VARCHAR(20),
-    CONSTRAINT pk_MethodePaiement PRIMARY KEY (idMethodePaiement),
-    CONSTRAINT ck_MethodePaiement CHECK (nomMethodePaiement IN ('Paypal', 'Visa', 'Mastercard', 'Carte_AE'))
+    idMethodePaiement INT AUTO_INCREMENT,
+    nomMethodePaiement VARCHAR(20) NOT NULL,
+    PRIMARY KEY (idMethodePaiement),
+    CHECK (nomMethodePaiement IN ('Paypal', 'Visa', 'MasterCard', 'Carte_AE'))
 );
 
 CREATE TABLE Paypal (
-    idcarte INT(6),
-    email VARCHAR(50),
-    MDP VARCHAR(25),
-    CONSTRAINT pk_Paypal PRIMARY KEY (idcarte)
+    idCarte INT AUTO_INCREMENT,
+    email VARCHAR(320) NOT NULL,
+    mdp VARCHAR(25) NOT NULL,
+    PRIMARY KEY (idCarte)
 );
 
 CREATE TABLE Carte_EU (
-    idcarte INT(6),
-    numCarte BIGINT(16),
-    dateExp INT(4),
-    nomProp VARCHAR(50),
-    CONSTRAINT pk_Carte_EU PRIMARY KEY (idcarte)
+    idCarte INT AUTO_INCREMENT,
+    numCarte BIGINT NOT NULL,
+    dateExp YEAR NOT NULL,
+    nomProp VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idCarte)
 );
 
 CREATE TABLE Carte_AE (
-    idcarte INT(6),
-    numCarte BIGINT(15),
-    dateExp INT(4),
-    nomProp VARCHAR(50),
-    CONSTRAINT pk_Carte_AE PRIMARY KEY (idcarte)
-    /* Note: MySQL cannot enforce pattern matching in CHECK constraints directly. */
-);
-
-CREATE TABLE Produit (
-    idNumProduit INT(6),
-    idTypeCategorie INT(3),
-    nomProduit VARCHAR(50),
-    prixActuel INT(5),
-    CONSTRAINT pk_Produit PRIMARY KEY (idNumProduit),
-    CONSTRAINT fk_Produit_Categorie FOREIGN KEY (idTypeCategorie) REFERENCES Categorie(idCategorie)
+    idCarte INT AUTO_INCREMENT,
+    numCarte VARCHAR(15) NOT NULL,
+    dateExp YEAR NOT NULL,
+    nomProp VARCHAR(50) NOT NULL,
+    PRIMARY KEY (idCarte),
+    CHECK (numCarte LIKE '34%' OR numCarte LIKE '37%')
 );
 
 CREATE TABLE Image (
-    idNumProduit INT(6),
-    idImage INT(6),
-    urlImage VARCHAR(100),
-    CONSTRAINT pk_Image PRIMARY KEY (idImage),
-    CONSTRAINT fk_Image_Produit FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
+    idImage INT AUTO_INCREMENT,
+    urlImage VARCHAR(100) NOT NULL,
+    PRIMARY KEY (idImage)
+);
+
+CREATE TABLE Categorie (
+    idCategorie INT AUTO_INCREMENT,
+    nomCategorie VARCHAR(30) NOT NULL,
+    idCategoriePere INT,
+    PRIMARY KEY (idCategorie),
+    FOREIGN KEY (idCategoriePere) REFERENCES Categorie(idCategorie)
+);
+
+CREATE TABLE Produit (
+    idNumProduit INT AUTO_INCREMENT,
+    idCategorie INT NOT NULL,
+    nomProduit VARCHAR(50) NOT NULL,
+    prixActuel DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (idNumProduit),
+    FOREIGN KEY (idCategorie) REFERENCES Categorie(idCategorie)
+);
+
+CREATE TABLE Contient (
+    idNumProduit INT NOT NULL,
+    idImage INT NOT NULL,
+    PRIMARY KEY (idNumProduit, idImage),
+    FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit),
+    FOREIGN KEY (idImage) REFERENCES Image(idImage)
 );
 
 CREATE TABLE Comporte (
-    idNumProduit1 INT(6),
-    idNumProduit2 INT(6),
-    CONSTRAINT pk_Comporte PRIMARY KEY (idNumProduit1, idNumProduit2),
-    CONSTRAINT fk_Comporte_Produit1 FOREIGN KEY (idNumProduit1) REFERENCES Produit(idNumProduit),
-    CONSTRAINT fk_Comporte_Produit2 FOREIGN KEY (idNumProduit2) REFERENCES Produit(idNumProduit)
+    idNumProduitComportant INT NOT NULL,
+    idNumProduitComporte INT NOT NULL,
+    PRIMARY KEY (idNumProduitComportant, idNumProduitComporte),
+    FOREIGN KEY (idNumProduitComportant) REFERENCES Produit(idNumProduit),
+    FOREIGN KEY (idNumProduitComporte) REFERENCES Produit(idNumProduit),
+    CHECK (idNumProduitComportant != idNumProduitComporte)
+);
+
+CREATE TABLE AdressePostale (
+    idAdresse INT AUTO_INCREMENT,
+    pays VARCHAR(30) NOT NULL,
+    ville VARCHAR(30) NOT NULL,
+    adr VARCHAR(50) NOT NULL,
+    codePostal VARCHAR(10) NOT NULL,
+    PRIMARY KEY (idAdresse)
+);
+
+CREATE TABLE Employe (
+    idNumEmploye INT AUTO_INCREMENT,
+    nom VARCHAR(25) NOT NULL,
+    prenom VARCHAR(15) NOT NULL,
+    idAdresse INT NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    telephone CHAR(10),
+    administrateur BOOLEAN NOT NULL,
+    PRIMARY KEY (idNumEmploye),
+    FOREIGN KEY (idAdresse) REFERENCES AdressePostale(idAdresse)
 );
 
 CREATE TABLE Client (
-    idNumCli INT(5),
-    nom VARCHAR(25),
-    prenom VARCHAR(15),
-    adressePostale VARCHAR(50),
-    email VARCHAR(20),
+    idNumCli INT AUTO_INCREMENT,
+    nom VARCHAR(25) NOT NULL,
+    prenom VARCHAR(15) NOT NULL,
+    idAdresse INT NOT NULL,
+    email VARCHAR(320) NOT NULL,
     telephone CHAR(10),
-    CONSTRAINT pk_Client PRIMARY KEY (idNumCli)
+    PRIMARY KEY (idNumCli),
+    FOREIGN KEY (idAdresse) REFERENCES AdressePostale(idAdresse)
 );
 
 CREATE TABLE infoPaiement (
-    idNumCli INT(5),
-    idcarte INT(6),
-    idMethodePaiement INT(6),
-    CONSTRAINT pk_infoPaiement PRIMARY KEY (idNumCli, idcarte, idMethodePaiement),
-    CONSTRAINT fk_infoPaiement_Client FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
-    CONSTRAINT fk_infoPaiement_MethodePaiement FOREIGN KEY (idMethodePaiement) REFERENCES MethodePaiement(idMethodePaiement),
-    CONSTRAINT fk_infoPaiement_Paypal FOREIGN KEY (idcarte) REFERENCES Paypal(idcarte),
-    CONSTRAINT fk_infoPaiement_Carte_EU FOREIGN KEY (idcarte) REFERENCES Carte_EU(idcarte),
-    CONSTRAINT fk_infoPaiement_Carte_AE FOREIGN KEY (idcarte) REFERENCES Carte_AE(idcarte)
-);
-
-CREATE TABLE MethodeEnregistrer (
-    idNumCli INT(5),
-    idMethodePaiement INT(6),
-    CONSTRAINT pk_MethodeEnregistrer PRIMARY KEY (idNumCli, idMethodePaiement),
-    CONSTRAINT fk_MethodeEnregistrer_Client FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
-    CONSTRAINT fk_MethodeEnregistrer_MethodePaiement FOREIGN KEY (idMethodePaiement) REFERENCES MethodePaiement(idMethodePaiement)
+    idNumCli INT NOT NULL,
+    idCarte INT NOT NULL,
+    idMethodePaiement INT NOT NULL,
+    PRIMARY KEY (idNumCli, idCarte, idMethodePaiement),
+    FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+    FOREIGN KEY (idMethodePaiement) REFERENCES MethodePaiement(idMethodePaiement)
 );
 
 CREATE TABLE Commande (
-    idCommande INT(6),
-    idNumCli INT(5),
-    idMethodePaiement INT(6),
-    dateCommande DATE,
-    panierActuel TINYINT(1),
-    CONSTRAINT pk_Commande PRIMARY KEY (idCommande),
-    CONSTRAINT fk_Commande_Client FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
-    CONSTRAINT fk_Commande_MethodePaiement FOREIGN KEY (idMethodePaiement) REFERENCES MethodePaiement(idMethodePaiement)
+    idCommande INT AUTO_INCREMENT,
+    idNumCli INT NOT NULL,
+    idAdresse INT NOT NULL,
+    idMethodePaiement INT NOT NULL,
+    dateCommande DATE NOT NULL,
+    panierActuel BOOLEAN NOT NULL,
+    PRIMARY KEY (idCommande),
+    FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+    FOREIGN KEY (idMethodePaiement) REFERENCES MethodePaiement(idMethodePaiement),
+    FOREIGN KEY (idAdresse) REFERENCES AdressePostale(idAdresse)
 );
 
 CREATE TABLE ACommande (
-    idCommande INT(6),
-    idNumProduit INT(6),
-    qte INT(5),
-    prixPayer INT(5),
-    CONSTRAINT pk_ACommande PRIMARY KEY (idCommande, idNumProduit),
-    CONSTRAINT fk_ACommande_Commande FOREIGN KEY (idCommande) REFERENCES Commande(idCommande),
-    CONSTRAINT fk_ACommande_Produit FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
+    idCommande INT NOT NULL,
+    idNumProduit INT NOT NULL,
+    qte INT NOT NULL,
+    prix DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (idCommande, idNumProduit),
+    FOREIGN KEY (idCommande) REFERENCES Commande(idCommande),
+    FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
 );
 
 CREATE TABLE ARegarde (
-    idNumCli INT(5),
-    idNumProduit INT(6),
-    dateRegar DATE,
-    CONSTRAINT pk_ARegarde PRIMARY KEY (idNumCli, idNumProduit),
-    CONSTRAINT fk_ARegarde_Client FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
-    CONSTRAINT fk_ARegarde_Produit FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
+    idNumCli INT NOT NULL,
+    idNumProduit INT NOT NULL,
+    dateConsultation DATE NOT NULL,
+    PRIMARY KEY (idNumCli, idNumProduit, dateConsultation),
+    FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+    FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
 );
 
 CREATE TABLE Avis (
-    idNumCli INT(5),
-    idNumProduit INT(6),
-    tAvis VARCHAR(500),
-    urlImage VARCHAR(100),
-    CONSTRAINT pk_Avis PRIMARY KEY (idNumCli, idNumProduit),
-    CONSTRAINT fk_Avis_Client FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
-    CONSTRAINT fk_Avis_Produit FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit)
+    idNumCli INT NOT NULL,
+    idNumProduit INT NOT NULL,
+    txtAvis VARCHAR(500),
+    idImage INT,
+    PRIMARY KEY (idNumCli, idNumProduit),
+    FOREIGN KEY (idNumCli) REFERENCES Client(idNumCli),
+    FOREIGN KEY (idNumProduit) REFERENCES Produit(idNumProduit),
+    FOREIGN KEY (idImage) REFERENCES Image(idImage)
 );
