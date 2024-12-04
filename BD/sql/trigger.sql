7fa71186d4695a5e9c79efc26c9a92702249a709
[titouan] <[titouannicolasgauze@gmail.com]>
2024-12-04 09:43:29 +0100

    ajout panier.css

--- BD/sql/trigger.sql
+++ BD/sql/trigger.sql
@@ -1,9 +1,22 @@
-CREATE OR REPLACE TRIGGER trg_check_numCarte
+DELIMITER $$
+
+CREATE TRIGGER t_iu_carte_AE_maj_nbd
 BEFORE INSERT OR UPDATE ON Carte_AE
 FOR EACH ROW
 BEGIN
     IF :NEW.numCarte NOT LIKE '34%' AND :NEW.numCarte NOT LIKE '37%' THEN
-        RAISE_APPLICATION_ERROR(-20001, 'numCarte doit commencer par "34" or "37".');
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'numCarte doit commencer par "34" ou "37".';
     END IF;
 END;
-/
+$$
+
+DELIMITER ;
+
+
+# faire que l'adresse email ne puisse pas être deux fois'
+ALTER TABLE Client
+ADD CONSTRAINT UNIQUE (email);
+
+# A consulter faire que quand produit ou categ cliquer : insert ARegarde
+INSERT INTO AConsulte (idNumCli, idNumProduit, dateConsultation) VALUES ((?), (?), CURRENT_DATE); 
